aad25baa4ba5c212693a095e831b9c60eb790758
Sandeep-FED <sandymankara11@gmail.com>
2022-08-30 22:37:38 +0530

    component: rendered images using props
    *styled images for md, lg screens
    
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,17 @@
 import React from "react"
 
-export default function Image() {
+export default function Image(props) {
   return (
     <>
       {console.log("hello")}
-      <h1>Image</h1>
+      <article className="shadow-md bg-white rounded-3xl p-5">
+        <img
+          src={props.urls.full}
+          alt={props.user.name}
+          loading="lazy"
+          className="h-60 w-full object-cover rounded-2xl md:h-80"
+        />
+      </article>
     </>
   )
 }
