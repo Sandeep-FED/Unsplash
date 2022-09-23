0c8cdfa025af5efca74f84d73d84ad82096767b2
Sandeep-FED <sandymankara11@gmail.com>
2022-09-23 17:33:50 +0530

    Style: cursor pointer

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,14 +3,15 @@ import React from "react"
 export default function Image(props) {
   return (
     <>
-      {console.log("hello")}
-      <article className="shadow-md bg-white rounded-3xl p-5">
+      {/* Image Card */}
+      <article className="shadow-md bg-white rounded-3xl p-5 cursor-pointer mb-5">
         <img
           src={props.urls.full}
           alt={props.user.name}
           loading="lazy"
           className="h-60 w-full object-cover rounded-2xl md:h-80"
         />
+        <div></div>
       </article>
     </>
   )
